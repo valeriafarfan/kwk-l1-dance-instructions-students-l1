1d3411cb954cef951d1cc0825764dfb63fd92544
valeria farfan <valeria073104@gmail.com>
2018-06-05 16:47:14 +0000

    automatically backed up by learn

--- method_argurments.rb
+++ method_argurments.rb
@@ -5,4 +5,12 @@ end
 #greeting
 #method called greeting.spits out whatever name i dessign,method has to be called at the end in order to return
 
-#greeting ("hey")
+#greeting ("hey","michelle","dagi","morning")
+#greeting
+#hey,dagi,im michelle,hows your morning?
+
+
+def greeting 
+  puts "#{local greeting} #{your name}, I'm  #{my name},hows your #{local_time}?"
+end
+greeting
