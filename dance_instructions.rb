4112d654db45c0f8ee797ae4e0d8b1da50a74578
valeria farfan <valeria073104@gmail.com>
2018-06-05 16:30:39 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,22 @@
-
+def stance 
+ puts "assume the proper stance"
+ puts "Plant legs far apart,bend knees slightly and keep posture loose"
+puts "BASE FOOTWORK"
+puts "1.)Lift right foot"
+puts "2.)Return right foot to the ground"
+puts "3.)Finishing with a small skip-step beackward"
+end
+stance
+def skip-step
+ puts "Skip Step Left"
+ puts "1.) lower left foot to the ground"
+ puts "2.)Bounce left foot back up slightly,kicking it a few inches back"
+ puts "3.)Left,Right,Left and Left"
+ puts "Skip Step Right"
+ puts "1.)Lower right foot to ground"
+ puts "2.)Bounce right foot back up slightly,kicking it a few inches back"
+ puts "Right,Left,Right and Right"
+ end
+ skip-step
+ def 
 # Code your methods below
